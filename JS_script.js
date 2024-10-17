44332d92206639b9b0466af7dd2ff84208b19f84
SweetyEmmanuvel <140629116+SweetyEmmanuvel@users.noreply.github.com>
2024-10-17 12:48:57 +0530

    Add files via upload

---
+++ JS_script.js
@@ -0,0 +1,11 @@
+ function displayingredients(){
+         var text=document.getElementById("ingredients");
+        text.style.display="block";
+ }
+
+ function displayinstructions(){
+     var text=document.getElementById("instructions");
+    text.style.display="block";
+ }
+
+   
